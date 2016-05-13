9777fb175ee1b8ea3a2c70fd007e5c718198d2e9
Joan Mon <jmon@foreride.com>
2016-05-13 12:43:35 +0200

    base tests

--- src/PhotoSuite/Domain/Exception/OutOfBoundsException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace PHPhotoSuit\PhotoSuite\Domain\Exception;
-
-class OutOfBoundsException extends \Exception
-{
-}
---
+++ tests/PhotoSuite/Domain/PhotoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PHPhotoSuit\Tests\PhotoSuite\Domain;
+
+class PhotoTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function dummy() {
+        $this->assertTrue(true);
+    }
+}
